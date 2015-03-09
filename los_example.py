d806cfd5eb22615a9eebdc73420941d0c6c9cb90
Kaitlin <kkgallagher9@gmail.com>
2015-03-09 12:24:58 -0400

    better example

--- los_example.py
+++ los_example.py
@@ -4,5 +4,7 @@ vis = los.Visibility()
 
 walls = [(0.0,0.0),(5.0,0.0),(5.0,6.0),(10.0,6.0),(10.0,10.0),(0.0,10.0)]
 vis.setup(walls)
-walls_seen = vis.run((3,3))
-print walls_seen
+while running:
+    current_position = (3,3)
+    walls_seen = vis.run(current_position)
+    print walls_seen
