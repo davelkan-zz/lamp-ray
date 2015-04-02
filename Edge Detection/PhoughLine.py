6a54aad67946ef33a98947345841f15ba3e23142
Davelkan <davelkan@Garyados.(none)>
2015-04-02 11:32:13 -0400

    finds corners and eliminates repetition with only 1 glitch

--- Edge Detection/PhoughLine.py
+++ Edge Detection/PhoughLine.py
@@ -7,73 +7,85 @@ import math
 
 class cornerFinder:
 	def __init__(self):
-		self.img = cv2.imread('map2.jpg')
+		self.img = cv2.imread('box.jpg')
 		gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
 		edges = cv2.Canny(gray,50,150,apertureSize = 3)
-		minLineLength = 1
-		maxLineGap = 1
+		self.minLineLength = 4
+		self.maxLineGap = 1
 		self.lineBox = []
+		self.longLines = []
 		self.slopeList = [[0,0,0,0]]
 		self.fullList = []
-		self.lines = cv2.HoughLinesP(edges,1,np.pi/180,5,minLineLength,maxLineGap)
+		self.filterList = []
+		self.sameList = []
+		self.lines = cv2.HoughLinesP(edges,1,np.pi/180,5,self.minLineLength,self.maxLineGap)
 		self.circuitComplete = False
+		print "self.lines", self.lines
 
+	''''buildLineBox works with no known bugs.
+		outputs lineBox, an array where each element
+		is a list representing each found line:
+		[slope, y or x intercept, length, 0]
+
+	'''
 	def buildLineBox(self):
 		for x1,y1,x2,y2 in self.lines[0]:
-			if x1 != x2:
-				m = (float(y1) - y2)/(x1-x2)
-				if m == -0:
-					m = 0
-				b = x1 - m*y1
-			else:
-				#arbitarilly set to 9001 so it isn't undefinedS
-				m = 9001
-				b = 9001
 			d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-			self.lineBox.append([m,b,d,0])
-			cv2.line(self.img,(x1,y1),(x2,y2),(0,255,0),2)
-		print "lineBox", self.lineBox
+			'''start by filtering out very short lines'''
+			if d > self.minLineLength:
+				self.longLines.append([x1,y1,x2,y2])
+				if x1 != x2:
+					m = (float(y1) - y2)/(x1-x2)
+					if m == -0:
+						m = 0
+					b = x1 - m*y1
+				else:
+					#arbitarilly set m 9001 so it isn't undefined, and set b to x-intercept
+					m = 9001
+					b = x1
+				self.lineBox.append([m,b,d,0])
+			#cv2.line(self.img,(x1,y1),(x2,y2),(0,255,0),2)
+		print "longLines(42)", self.longLines
+		#print "linBox Size(36)", len(self.lineBox)
+		#cv2.imwrite('map5.jpg',self.img)
+		self.compareLines()
+
+
+	#filterList [[index1,index2,x1_comparison,y1_comparison,21_comparison,y2_comparison],[index1,index2,x1_comparison,y1_comparison,x2_comparison,y2_comparison]...]
+	def compareLines(self):
+		for index,item in enumerate(self.longLines):
+			for index2,item2 in enumerate(self.longLines):
+				if index < index2:
+					same = [index,index2]
+					for index3,value in enumerate(item):
+						#they are the same line
+						if abs(value - item2[index3]) < 6:
+							same.append(True)
+
+						#they are not the same line
+						else:
+							same.append(False)
+					if False not in same:
+						self.sameList.append([index,index2])
+					self.filterList.append(same)
+		#print self.filterList
+		print self.sameList
 		self.filterLines()
 
-
-
-#lines identified! Now i must check for overlaps!!!
-
-#build the ranking
 	def filterLines(self):
-		for index,i in enumerate(self.lineBox):
-			islope = i[0]
-			intercept = i[1]
-
-			for jindex,j in enumerate(self.slopeList):
-				#print jindex
-				#print "intercept difference", abs(j[1] - i[1])
-				'''#to expand range modify the boundaries'''
-				if abs(j[0] - i[0]) < 0.25 and  0.9 < float(j[1])/i[1] < 1.1:
-					if j[2] > i[2]:
-						self.fullList[jindex][2] = j[2]
-						self.fullList[jindex][3] = index
-					else:
-						self.fullList[jindex][2] = i[2]
-						self.fullList[jindex][3] = index
-				elif jindex == len(self.slopeList)-1:
-					i[3] = index
-					self.fullList.append(i)
-			#print "slopeList", slopeList
-			#print "fullList", fullList
-			self.slopeList = self.fullList
-		print "fullList", self.fullList
-		self.createList()
-
-	def createList(self):
-		#now finally extracting the given index
-		finalList = []
-		for index,item in enumerate(self.fullList):
-			print "index", item[3]
-			finalList.append(self.lines[0][item[3]])
-
-		print "finalList", finalList
-		cv2.imwrite('map5.jpg',self.img)
+		pairedIndex = []
+		for index,element in enumerate(self.sameList):
+			pairedIndex.append(self.sameList[index][0])
+			pairedIndex.append(self.sameList[index][1])
+			if self.lineBox[element[0]] > self.lineBox[element[1]]:
+				self.fullList.append(self.longLines[element[0]])
+			else:
+				self.fullList.append(self.longLines[element[1]])
+		print "pairedIndex",pairedIndex
+		for i in range(len(self.longLines)):
+			if i not in pairedIndex:
+				self.fullList.append(self.longLines[i])
+		print "self.fullList",self.fullList
 		self.circuitComplete = True
 
 	def run(self):
